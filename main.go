a59869a5c83528d5e24c29e68726854094e1b0c8
Nihar Dangi <nihardangi@gmail.com>
2025-06-28 13:44:09 +0530

    code cleanup

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	latestPriceFeedData, _ := priceFeed.FetchLatestPriceFeedData(priceFeedIDs)
 	fmt.Println("latest feed data----------", latestPriceFeedData)
 
-	updatePriceIDs := []string{"0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43"}
-	priceFeed.PrepareDataForUpdatePriceFeeds(latestPriceFeedData, updatePriceIDs)
+	updatePriceFeedIDs := []string{"0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43"}
+	priceFeed.PrepareDataForUpdatePriceFeeds(latestPriceFeedData, updatePriceFeedIDs)
 }
